f2145e6e65697f84d6e6036c49d8101b083e3fc1
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2025-07-22 08:33:26 +0700

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -15,6 +15,18 @@ from pymongo.server_api import ServerApi
 import pandas as pd
 import numpy as np
 st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="Dashboard Gait Analysis")
+st.markdown(
+    """
+    <style>
+    .block-container {
+        zoom: 0.75;
+        -moz-transform: scale(0.75);
+        -moz-transform-origin: top left;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 class GaitAnalysisData:
     def __init__(self, data):
         self.df = pd.read_excel(data, sheet_name=[0, 1])  # Read the uploaded file
